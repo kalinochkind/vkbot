8bb255740ef6781339c1529c78babe0f329279ca
Denis Kalinochkin <kalinochkind@gmail.com>
2015-12-20 22:14:52 +0300

    Remove friends not allowing to send private messages

--- a.py
+++ a.py
@@ -8,21 +8,26 @@ a = vk_api(login, password, 10)
 a.delayedReset()
 friends = []
 print('Fetching friends')
-
+noadd = check_friend.noadd
 for i in range(1000000):
     print('page', i)
-    fr = a.friends.get(fields=check_friend.fields, count=1000, offset=i*1000)
+    fr = a.friends.get(fields=check_friend.fields+',can_write_private_message', count=1000, offset=i*1000)
     friends.extend(fr['items'])
     if len(fr['items']) < 1000:
         break
 
 print('Starting to delete')
 for i in friends:
+    if not i['can_write_private_message']:
+        noadd.add(str(i['id']))
     if not check_friend.is_good(i):
         a.friends.delete.delayed(user_id=i['id'])
         print('deleted', i['id'])
 a.sync()
 print()
+
+with open('noadd.txt', 'w') as f:
+    f.write('\n'.join(noadd))
 
 print('Fetching followers')
 foll = []
--- check_friend.py
+++ check_friend.py
@@ -4,6 +4,7 @@ import config
 fields = 'photo_50,country,last_seen'
 
 s = open('allowed.txt').readlines()
+noadd = set(open('noadd.txt').read().split())
 allowed = set(s[0] + ' ')
 s = s[1].split()
 
@@ -19,5 +20,6 @@ def is_good(fr):
             fr.get('country', {'id':0})['id'] in [0, 1, 2, 3] and 
             all(check_char(i) for i in fr['first_name'] + fr['last_name']) and
             now - fr['last_seen']['time'] < 3600 * 24 * offline_allowed and
-            not any(i in (fr['first_name'] + ' ' + fr['last_name']).lower() for i in s)
+            not any(i in (fr['first_name'] + ' ' + fr['last_name']).lower() for i in s) and
+            str(fr['id']) not in noadd
             )
