2e6007fa2de6462f38afdebdb95295509410dc23
Denis Kalinochkin <kalinochkind@gmail.com>
2016-02-13 22:38:16 +0300

    Fix for building not as root

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 g++ main.cpp ChatBot.cpp Analyzer.cpp -O3 -march=atom -fno-exceptions -fno-stack-limit -W -Wshadow -Winline -Wdisabled-optimization -Wredundant-decls -Wunreachable-code -Wall -Wextra -Weffc++ -pedantic -pipe -fomit-frame-pointer -ffast-math -fno-rtti --std=c++11 -o chat.exe
+chmod a+w chat.exe
