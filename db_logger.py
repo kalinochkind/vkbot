34f83765d850fbc97cc3512eac4c2ebab551b5f7
Denis Kalinochkin <kalinochkind@gmail.com>
2016-03-15 17:02:44 +0300

    Connect to MySQL only when needed

--- db_logger.py
+++ db_logger.py
@@ -3,13 +3,18 @@ import config
 import threading
 
 enabled = False
+connected = False
 
 db_lock = threading.Lock()
-conn = mysql.connector.connect(host=config.get('db_logger.host'), user=config.get('db_logger.username'), password=config.get('db_logger.password'), database=config.get('db_logger.database'))
-cur = conn.cursor()
+
 
 def log(message, kind):
     if enabled:
         with db_lock:
+            global conn, cur, connected
+            if not connected:
+                conn = mysql.connector.connect(host=config.get('db_logger.host'), user=config.get('db_logger.username'), password=config.get('db_logger.password'), database=config.get('db_logger.database'))
+                cur = conn.cursor()
+                connected = True
             cur.execute('INSERT INTO vkbot_logmessage VALUES (NULL, %s, %s, NOW())', (message, kind))
             conn.commit()
