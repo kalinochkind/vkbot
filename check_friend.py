27523dbd330e80d39f698c7df538b705b26a248a
Denis Kalinochkin <kalinochkind@gmail.com>
2016-06-21 00:47:04 +0300

    Bugfix

--- check_friend.py
+++ check_friend.py
@@ -28,7 +28,7 @@ checks = [
 (lambda fr:fr['photo_50'] and not fr['photo_50'].endswith('camera_50.png'), 'No avatar'),
 (lambda fr:fr.get('country', {'id':0})['id'] in [0, 1, 2, 3], 'Bad country'),
 (lambda fr:all(check_char(i) for i in fr['first_name'] + fr['last_name']), 'Bad characters in name'),
-(lambda fr:time.time() - fr['last_seen']['time'] < 3600 * 24 * offline_allowed, 'Offline too long'),
+(lambda fr:'last_seen' in fr and time.time() - fr['last_seen']['time'] < 3600 * 24 * offline_allowed, 'Offline too long'),
 (lambda fr:not any(i in (fr['first_name'] + ' ' + fr['last_name']).lower() for i in s), 'Bad substring in name'),
 (lambda fr:fr['id'] not in noadd, 'Ignored'),
 (lambda fr:fr['first_name'] != fr['last_name'], 'First name equal to last name'),
