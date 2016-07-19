e31d6560d870b4449171876cbbe63d064bbd1666
Denis Kalinochkin <kalinochkind@gmail.com>
2016-07-19 23:03:56 +0300

    Treat graffiti as photos

--- vkbot.py
+++ vkbot.py
@@ -143,6 +143,11 @@ class vk_bot:
                         del opt['attach{}_type'.format(i)]
                         del opt['attach{}'.format(i)]
                         text += '.. '
+                    if opt.get('attach{}_type'.format(i)) == 'doc' and opt.get('attach{}_kind'.format(i)) == 'graffiti':
+                        del opt['attach{}_type'.format(i)]
+                        del opt['attach{}'.format(i)]
+                        del opt['attach{}_kind'.format(i)]
+                        text += '.. '
                 if  not (set(opt) <= {'from', 'emoji'} or opt.get('attach1_type') == 'sticker'):
                     need_extra.append(str(mid))
                     continue
