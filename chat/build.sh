9c369cb9ddf47bcfd2ffe0edf89e1f9a0dfaa95a
Denis Kalinochkin <kalinochkind@gmail.com>
2016-07-27 14:02:29 +0300

    native

--- chat/build.sh
+++ chat/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"
-g++ main.cpp ChatBot.cpp Analyzer.cpp -O3 -march=atom -fno-exceptions -fno-stack-limit -W -Wshadow -Winline -Wdisabled-optimization -Wredundant-decls -Wunreachable-code -Wall -Wextra -Weffc++ -pedantic -pipe -fomit-frame-pointer -ffast-math -fno-rtti --std=c++11 -o chat.exe
+g++ main.cpp ChatBot.cpp Analyzer.cpp -O3 -march=native -fno-exceptions -fno-stack-limit -W -Wshadow -Winline -Wdisabled-optimization -Wredundant-decls -Wunreachable-code -Wall -Wextra -Weffc++ -pedantic -pipe -fomit-frame-pointer -ffast-math -fno-rtti --std=c++11 -o chat.exe
 chmod a+w chat.exe
