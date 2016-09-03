26bd91014457cd5d07e7764f7be136d31848dcc8
Denis Kalinochkin <kalinochkind@gmail.com>
2016-09-03 15:34:20 +0300

    Lock for stats

--- stats.py
+++ stats.py
@@ -1,6 +1,8 @@
 import json
 import accounts
+import threading
 
+lock = threading.Lock()
 
 try:
     _stats = json.loads(open(accounts.getFile('stats.txt')).read())
@@ -8,11 +10,13 @@ except Exception:
     _stats = {}
 
 def update(name, value):
-    if name in _stats and _stats[name] == value:
-        return
-    _stats[name] = value
-    with open(accounts.getFile('stats.txt'), 'w') as f:
-        f.write(json.dumps(_stats))
+    with lock:
+        if name in _stats and _stats[name] == value:
+            return
+        _stats[name] = value
+        with open(accounts.getFile('stats.txt'), 'w') as f:
+            f.write(json.dumps(_stats))
 
 def get(name, default=None):
-    return _stats.get(name, default)
+    with lock:
+        return _stats.get(name, default)
