a74f0b5595380825ba87af50d14a68e749157e1d
Denis Kalinochkin <kalinochkind@gmail.com>
2016-10-08 20:19:59 +0300

    Disable logging from requests

--- prepare.py
+++ prepare.py
@@ -38,6 +38,7 @@ class MyHandler(logging.Handler):
 
 logging.basicConfig(handlers=[MyHandler()], level=logging.INFO)
 logging.getLogger('antigate').setLevel(logging.CRITICAL)
+logging.getLogger('requests').setLevel(logging.CRITICAL)
 
 os.environ['LC_ALL'] = 'ru_RU.utf-8'
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
