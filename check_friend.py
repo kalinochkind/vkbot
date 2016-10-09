84ee309daaaded6334d9b34bfe5bc56d9441736e
Denis Kalinochkin <kalinochkind@gmail.com>
2016-10-09 15:12:36 +0300

    add_everyone option

--- check_friend.py
+++ check_friend.py
@@ -12,7 +12,7 @@ allowed = set(line1 + ' ')
 banned_substrings = line2.split()
 
 offline_allowed = config.get('check_friend.offline_allowed', 'i')
-
+add_everyone = config.get('vkbot.add_everyone')
 
 def writeNoadd():
     with open(accounts.getFile('noadd.txt'), 'w') as f:
@@ -28,20 +28,20 @@ def appendNoadd(users):
 
 
 checks = [
+    (lambda fr: fr['id'] not in noadd, 'Ignored'),
     (lambda fr: 'deactivated' not in fr, 'Account is deactivated'),
     (lambda fr: fr['photo_50'] and not fr['photo_50'].endswith('camera_50.png'), 'No avatar'),
     (lambda fr: fr.get('country', {'id': 0})['id'] in [0, 1, 2, 3], 'Bad country'),
     (lambda fr: all(i in allowed for i in fr['first_name'] + fr['last_name']), 'Bad characters in name'),
     (lambda fr: not fr.get('last_seen') or time.time() - fr['last_seen']['time'] < 3600 * 24 * offline_allowed, 'Offline too long'),
     (lambda fr: not any(i in (fr['first_name'] + ' ' + fr['last_name']).lower() for i in banned_substrings), 'Bad substring in name'),
-    (lambda fr: fr['id'] not in noadd, 'Ignored'),
     (lambda fr: fr['first_name'] != fr['last_name'], 'First name equal to last name'),
 ]
 
 
 def isGood(fr, need_reason=False):
     reasons = []
-    for fun, msg in checks:
+    for fun, msg in (checks[:1] if add_everyone else checks):
         if not fun(fr):
             if need_reason:
                 reasons.append(msg.lower() if reasons else msg)
