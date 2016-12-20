8684d1d58182937d0e4dc5f31b2688cb318feed8
Denis Kalinochkin <kalinochkind@gmail.com>
2016-12-20 11:37:20 +0300

    Ignore duplicates

--- scripts/like.py
+++ scripts/like.py
@@ -19,10 +19,15 @@ def main(a, args):
         return
     data = a.wall.get(owner_id=uid, count=100, extended=1)
     profiles = {i['id']: i for i in data['profiles']}
+    liked = set()
     for i in data['items']:
         if sex and i['from_id'] in profiles and profiles[i['from_id']]['sex'] != sex:
             logging.info('Skip ' + str(i['id']))
             continue
+        if i['from_id'] in liked:
+            logging.info('Duplicate ' + str(i['id']))
+            continue
+        liked.add(i['from_id'])
         logging.info('Like ' + str(i['id']))
         a.likes.add(owner_id=i['owner_id'], item_id=i['id'], type='post')
         time.sleep(3)
