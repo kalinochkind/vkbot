7122f17d4ee1220d367d7749484975ec04c94e74
Denis Kalinochkin <kalinochkind@gmail.com>
2016-12-24 16:55:16 +0300

    Error if config parameter does not exist

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import configparser
 import accounts
+import sys
 
 cp = configparser.ConfigParser()
 cp.read(accounts.getFile('inf.cfg'))
@@ -7,11 +8,15 @@ cp.read(accounts.getFile('inf.cfg'))
 
 def get(param, typename='s'):
     param = param.split('.')
-    if typename == 's':
-        return cp[param[0]].get(param[1])
-    elif typename == 'i':
-        return cp[param[0]].getint(param[1])
-    elif typename == 'f':
-        return cp[param[0]].getfloat(param[1])
-    elif typename == 'b':
-        return cp[param[0]].getboolean(param[1])
+    try:
+        if typename == 's':
+            return cp[param[0]].get(param[1])
+        elif typename == 'i':
+            return cp[param[0]].getint(param[1])
+        elif typename == 'f':
+            return cp[param[0]].getfloat(param[1])
+        elif typename == 'b':
+            return cp[param[0]].getboolean(param[1])
+    except KeyError:
+        print('Parameter {}.{} does not exist'.format(*param))
+        sys.exit()
