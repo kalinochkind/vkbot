26f092dd48cb31cf46e18b82192c7d5fa39d1880
Denis Kalinochkin <kalinochkind@gmail.com>
2017-01-21 20:54:20 +0300

    Bugfix

--- scripts/update_friends.py
+++ scripts/update_friends.py
@@ -6,6 +6,9 @@ import scriptlib
 
 # noinspection PyUnusedLocal
 def main(a, args):
+    a.ignored_errors = {
+        (177, 'friends.add'): None,
+    }
     a.timeout = 10
     banned = list(map(int, open(accounts.getFile('banned.txt')).read().split()))
     controller = scriptlib.createFriendController()
@@ -17,13 +20,14 @@ def main(a, args):
             a.friends.delete.delayed(user_id=i['id'])
             logging.info('deleted ' + str(i['id']))
             log.write('_update_friends', 'deleted ' + str(i['id']))
+    a.sync()
 
     foll = scriptlib.getFollowers(a, fields=controller.fields)
-
     logging.info('Starting to add')
+    def cb(req, resp):
+        logging.info(('added ' if resp else 'error ') + str(req['user_id']))
     for i in foll:
         if controller.isGood(i):
-            a.friends.add.delayed(user_id=i['id'])
-            logging.info('added ' + str(i['id']))
-            log.write('_update_friends', 'added ' + str(i['id']))
+            a.friends.add.delayed(user_id=i['id']).callback(cb)
+    a.sync()
     logging.info('\nFinished')
