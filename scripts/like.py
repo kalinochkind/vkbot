6a21fa73cbb6c63fd2483acaa9592e6794f18a64
Denis Kalinochkin <kalinochkind@gmail.com>
2017-02-01 02:44:25 +0300

    Some change

--- scripts/like.py
+++ scripts/like.py
@@ -28,13 +28,13 @@ def main(a, args):
         if sex and i['from_id'] in profiles and profiles[i['from_id']]['sex'] != sex:
             logging.info('Skip ' + str(i['id']))
             continue
-        if profiles[i['from_id']]['blacklisted'] or profiles[i['from_id']]['blacklisted_by_me']:
-            logging.info('Blacklist ' + str(i['id']))
-            continue
         if i['from_id'] in liked:
             logging.info('Duplicate ' + str(i['id']))
             continue
         liked.add(i['from_id'])
+        if profiles[i['from_id']]['blacklisted'] or profiles[i['from_id']]['blacklisted_by_me']:
+            logging.info('Blacklist ' + str(i['id']))
+            continue
         logging.info('Like ' + str(i['id']))
         a.likes.add(owner_id=i['owner_id'], item_id=i['id'], type='post')
         time.sleep(3)
