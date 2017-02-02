78f9a5128c25e67965ac1f6810c30819c6e8b06c
Denis Kalinochkin <kalinochkind@gmail.com>
2017-02-02 19:17:54 +0300

    PostgreSQL

--- db_logger.py
+++ db_logger.py
@@ -9,8 +9,7 @@ import log as _log
 MAX_TEXT_LENGTH = 1024
 
 enabled = bool(args.args['database'])
-if enabled:
-    import MySQLdb
+engine = None
 connected = False
 conn = None
 cur = None
@@ -18,31 +17,60 @@ cur = None
 db_lock = threading.RLock()
 
 def _connect():
-    global conn, cur, connected
+    global conn, cur, connected, enabled
+    if engine == 'mysql':
+        import MySQLdb
+    elif engine == 'postgresql':
+        import psycopg2
     if not connected:
-        conn = MySQLdb.connect(host=config.get('db_logger.host'), user=config.get('db_logger.username'), password=config.get('db_logger.password'),
-                               database=config.get('db_logger.database'), charset='utf8mb4')
+        if engine == 'mysql':
+            conn = MySQLdb.connect(host=config.get('db_logger.host'), user=config.get('db_logger.username'),
+                                   password=config.get('db_logger.password'), database=config.get('db_logger.database'), charset='utf8mb4')
+        else:
+            conn = psycopg2.connect("dbname='{}' user='{}' host='{}' password='{}'".format(config.get('db_logger.database'),
+                                    config.get('db_logger.username'), config.get('db_logger.host'), config.get('db_logger.password')))
         cur = conn.cursor()
         connected = True
 
 def log(message, kind, text_msg=None):
-    global connected, enabled
+    global connected, enabled, engine
+    if enabled:
+        engine = config.get('db_logger.engine')
     if enabled:
         if not config.get('db_logger.host') or not config.get('db_logger.database'):
             print('Incorrect database configuration!')
             enabled = False
             return
+        if text_msg is None:
+            text_msg = message
+        text_msg = text_msg[:MAX_TEXT_LENGTH]
         with db_lock:
-            try:
-                _connect()
-                if text_msg is None:
-                    text_msg = message
-                text_msg = text_msg[:MAX_TEXT_LENGTH]
-                cur.execute('INSERT INTO vkbot_logmessage VALUES (NULL, %s, %s, NOW(), %s, %s)', (message, kind, text_msg, accounts.current_account))
-                conn.commit()
-            except MySQLdb.Error as e:
-                print(e, flush=True)
-                _log.write('error', 'MySQL error: ' + str(e))
-                time.sleep(5)
-                connected = False
-                log(message, kind, text_msg)
+            if engine == 'mysql':
+                import MySQLdb
+                try:
+                    _connect()
+                    cur.execute('INSERT INTO vkbot_logmessage VALUES (NULL, %s, %s, NOW(), %s, %s)',
+                                (message, kind, text_msg, accounts.current_account))
+                    conn.commit()
+                except MySQLdb.Error as e:
+                    print(e, flush=True)
+                    _log.write('error', 'MySQL error: ' + str(e))
+                    time.sleep(5)
+                    connected = False
+                    log(message, kind, text_msg)
+            elif engine == 'postgresql':
+                import psycopg2
+                try:
+                    _connect()
+                    cur.execute('INSERT INTO vkbot_logmessage(message, kind, "time", message_text) VALUES (%s, %s, NOW(), %s)', (message, kind, text_msg))
+                    conn.commit()
+                except psycopg2.Error as e:
+                    print(e, flush=True)
+                    _log.write('error', 'PostgreSQL error: ' + str(e))
+                    time.sleep(5)
+                    connected = False
+                    log(message, kind, text_msg)
+            else:
+                print('Unknown database engine')
+                enabled = False
+                return
