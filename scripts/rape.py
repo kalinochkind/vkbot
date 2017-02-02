6fa0befa3d3224c3d84cf806ad23459ec53f8fd1
Denis Kalinochkin <kalinochkind@gmail.com>
2017-02-03 00:02:13 +0300

    Script to invade chats

---
+++ scripts/rape.py
@@ -0,0 +1,20 @@
+import vkapi
+need_auth = True
+
+
+def main(a, args):
+    if not args:
+        print('Config file required')
+        return
+    data = open(args[0]).read().split()
+    self_id = str(a.users.get()[0]['id'])
+    print('My id:', self_id, flush=True)
+    while True:
+        lp = a.getLongpoll()
+        for i in lp:
+            if i[0] != 4:
+                continue
+            mid, flags, sender, ts, random_id, text, opt = i[1:]
+            if opt.get('source_act') == 'chat_kick_user' and opt['source_mid'] in data and opt['source_mid'] != self_id:
+                print('Adding', opt['source_mid'], flush=True)
+                a.messages.addChatUser(user_id=opt['source_mid'], chat_id=sender - vkapi.CONF_START)
