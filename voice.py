6dda1324838e8962fb6bea0c11e98707cd4d6246
Denis Kalinochkin <kalinochkind@gmail.com>
2019-05-02 00:03:04 +0300

    Handle wit errors

--- voice.py
+++ voice.py
@@ -10,10 +10,17 @@ class VoiceRecognizer:
         self.wit = Wit(wit_token)
 
     def get_text(self, mp3_link):
+        from wit.wit import WitError
         try:
             logger.info('Recognizing voice message')
-            with urllib.request.urlopen(mp3_link) as f:
-                return self.wit.speech(f, None, {'Content-type': 'audio/mpeg'})['_text']
+            for i in range(3):
+                try:
+                    with urllib.request.urlopen(mp3_link) as f:
+                        return self.wit.speech(f, None, {'Content-type': 'audio/mpeg'})['_text']
+                except WitError:
+                    if i == 2:
+                        raise
+                    logger.warning('Wit error, retrying')
         except Exception:
             logger.exception('Voice recognition error')
             return None
